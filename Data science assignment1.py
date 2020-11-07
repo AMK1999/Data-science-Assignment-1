a5c2f40669c8fdf286fcf61d50b3b6d840851865
AMK1999 <74068352+AMK1999@users.noreply.github.com>
2020-11-07 18:55:41 +0530

    Add files via upload

---
+++ Data science assignment1.py
@@ -0,0 +1,19 @@
+#Ans1:- RAINBOW
+
+#Ans2:-
+print('LETS UPGRADE')
+
+#Ans3:-
+cost=int(input('cost price:'))
+sell=int(input('Selling price:'))
+if cost>sell:
+    print('Loss')
+elif cost<sell:
+    print('Profit')
+else:
+    print('Neither')
+
+#Ans4:
+num1=int(input('Input:'))
+c=num1*80
+print('output:',c)
